50cf6fb155da5624ba7740c5f6629fcba24a15ed
masihur Rohman <masihur@evidentbd.com>
2026-03-17 10:50:43 +0600

    Login To Dashboard navigate

--- lib/core/constants/api_path.dart
+++ lib/core/constants/api_path.dart
@@ -1,6 +1,7 @@
 class APIPath{
   //Base Url for HR Service
-  static const String baseUrl = 'https://smart-school-backend-mmy3.onrender.com';
+  // static const String baseUrl = 'https://smart-school-backend-mmy3.onrender.com';
+  static const String baseUrl = 'http://192.168.68.120:3000';
   static String login = "$baseUrl/auth/login";
   static String register = "$baseUrl/users";
   static String profile = "$baseUrl/auth/profile";
