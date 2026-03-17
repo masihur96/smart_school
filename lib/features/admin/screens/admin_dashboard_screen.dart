5a65b760b03dba61fbc383d54257a48cb89d35d9
masihur Rohman <masihur@evidentbd.com>
2026-03-17 10:01:16 +0600

    Login To Dashboard navigate

--- lib/features/admin/screens/admin_dashboard_screen.dart
+++ lib/features/admin/screens/admin_dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:smart_school/features/profile/presentation/views/profile_screen.dart';
 import 'package:smart_school/models/school_models.dart';
 import '../../../core/widgets/app_drawer.dart';
 import '../../teacher/providers/attendance_provider.dart';
@@ -64,7 +65,10 @@ class _AdminDashboardScreenState extends State<AdminDashboardScreen> {
         actions: [
           IconButton(
             icon: const Icon(Icons.account_circle),
-            onPressed: () => context.push('/profile'),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (_)=>ProfileScreen(),),);
+
+            },
           ),
           const SizedBox(width: 8),
         ],
