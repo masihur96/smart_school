9625bcdefba98a0835bf74f090b366073d616af9
masihur <masihur96@gmail.com>
2026-04-11 16:42:35 +0600

    Super Admin Dashboard

---
+++ lib/features/super_admin/screens/backup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BackupScreen extends StatefulWidget {
+  const BackupScreen({super.key});
+
+  @override
+  State<BackupScreen> createState() => _BackupScreenState();
+}
+
+class _BackupScreenState extends State<BackupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/super_admin/screens/super_admin_school_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SuperAdminSchoolScreen extends StatefulWidget {
+  const SuperAdminSchoolScreen({super.key});
+
+  @override
+  State<SuperAdminSchoolScreen> createState() => _SuperAdminSchoolScreenState();
+}
+
+class _SuperAdminSchoolScreenState extends State<SuperAdminSchoolScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
