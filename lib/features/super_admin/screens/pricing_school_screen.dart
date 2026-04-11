1f0761e3f83cd26947b168994fc18d881ece266f
masihur <masihur96@gmail.com>
2026-04-11 16:42:35 +0600

    Super Admin Dashboard

---
+++ lib/features/super_admin/screens/pricing_school_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PricingSchoolScreen extends StatefulWidget {
+  const PricingSchoolScreen({super.key});
+
+  @override
+  State<PricingSchoolScreen> createState() => _PricingSchoolScreenState();
+}
+
+class _PricingSchoolScreenState extends State<PricingSchoolScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/features/super_admin/screens/super_admin_dashboard_screen.dart
+++ lib/features/super_admin/screens/super_admin_dashboard_screen.dart
@@ -4,6 +4,7 @@ import 'package:smart_school/core/theme/app_colors.dart';
 import 'package:smart_school/core/widgets/app_drawer.dart';
 import 'package:smart_school/features/auth/providers/auth_provider.dart';
 import 'package:smart_school/features/super_admin/providers/super_admin_dashboard_provider.dart';
+import 'package:smart_school/features/super_admin/screens/super_admin_school_screen.dart';
 import 'package:smart_school/models/user_model.dart';
 
 class SuperAdminDashboardScreen extends StatefulWidget {
@@ -405,7 +406,12 @@ class _SuperAdminDashboardScreenState extends State<SuperAdminDashboardScreen> {
             'Schools',
             Icons.add_business_rounded,
             Colors.indigo,
-            () {},
+            () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => SuperAdminSchoolScreen()),
+              );
+            },
           ),
           _buildActionItem(
             'Pricing',
