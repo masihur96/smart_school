176206b3212c0ecee39605e9a0bb9a748e3d0031
masihur <masihur96@gmail.com>
2026-04-13 09:12:33 +0600

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:smart_school/configs/route_generator.dart';
 import 'package:smart_school/l10n/app_localizations.dart';
+
+import 'configs/network/data_provider.dart';
 import 'core/theme/app_theme.dart';
-import 'features/auth/providers/auth_provider.dart';
-import 'features/admin/providers/notice_provider.dart';
-import 'features/admin/providers/student_provider.dart';
-import 'features/admin/providers/setup_provider.dart';
 import 'features/admin/providers/exam_provider.dart';
-import 'features/admin/providers/teacher_provider.dart';
+import 'features/admin/providers/notice_provider.dart';
 import 'features/admin/providers/routine_provider.dart';
 import 'features/admin/providers/settings_provider.dart';
-
-import 'features/teacher/providers/homework_provider.dart';
-import 'features/teacher/providers/attendance_provider.dart';
-import 'features/teacher/providers/result_provider.dart';
-import 'features/teacher/providers/teacher_dashboard_provider.dart';
-import 'features/teacher/providers/teacher_attendance_provider.dart';
-import 'features/teacher/data/repositories/attendance_repository_impl.dart';
-import 'features/teacher/data/repositories/result_repository_impl.dart';
-import 'features/teacher/data/datasources/homework_remote_datasource.dart';
-import 'features/teacher/data/datasources/mark_entry_remote_datasource.dart';
-import 'features/teacher/data/repositories/homework_repository_impl.dart';
-import 'features/student/providers/student_attendance_provider.dart';
-import 'features/student/providers/student_routine_provider.dart';
-import 'features/student/providers/student_homework_provider.dart';
-import 'features/student/providers/student_result_provider.dart';
-import 'features/super_admin/providers/super_admin_dashboard_provider.dart';
-import 'features/super_admin/providers/super_admin_school_provider.dart';
-import 'features/super_admin/providers/pricing_notifier.dart';
-import 'features/super_admin/providers/subscription_provider.dart';
-import 'features/super_admin/providers/trash_restore_provider.dart';
-import 'services/database_service.dart';
-import 'configs/network/data_provider.dart';
+import 'features/admin/providers/setup_provider.dart';
+import 'features/admin/providers/student_provider.dart';
+import 'features/admin/providers/teacher_provider.dart';
 import 'features/auth/data/datasources/auth_remote_data_source.dart';
 import 'features/auth/data/repositories/auth_repository_impl.dart';
+import 'features/auth/domain/usecases/change_password_usecase.dart';
+import 'features/auth/domain/usecases/get_profile_usecase.dart';
 import 'features/auth/domain/usecases/login_usecase.dart';
 import 'features/auth/domain/usecases/register_usecase.dart';
-import 'features/auth/domain/usecases/get_profile_usecase.dart';
-import 'features/auth/domain/usecases/change_password_usecase.dart';
+import 'features/auth/providers/auth_provider.dart';
+import 'features/student/providers/student_attendance_provider.dart';
+import 'features/student/providers/student_homework_provider.dart';
+import 'features/student/providers/student_result_provider.dart';
+import 'features/student/providers/student_routine_provider.dart';
+import 'features/super_admin/providers/pricing_notifier.dart';
+import 'features/super_admin/providers/subscription_provider.dart';
+import 'features/super_admin/providers/super_admin_dashboard_provider.dart';
+import 'features/super_admin/providers/super_admin_school_provider.dart';
+import 'features/super_admin/providers/trash_restore_provider.dart';
+import 'features/teacher/data/datasources/homework_remote_datasource.dart';
+import 'features/teacher/data/datasources/mark_entry_remote_datasource.dart';
+import 'features/teacher/data/repositories/attendance_repository_impl.dart';
+import 'features/teacher/data/repositories/homework_repository_impl.dart';
+import 'features/teacher/data/repositories/result_repository_impl.dart';
+import 'features/teacher/providers/attendance_provider.dart';
+import 'features/teacher/providers/homework_provider.dart';
+import 'features/teacher/providers/result_provider.dart';
+import 'features/teacher/providers/teacher_attendance_provider.dart';
+import 'features/teacher/providers/teacher_dashboard_provider.dart';
+import 'firebase_options.dart';
+import 'services/database_service.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final databaseService = DatabaseService();
   final dataProvider = DataProvider();
 
@@ -66,8 +69,10 @@ void main() {
   final homeworkRepository = HomeworkRepositoryImpl(homeworkRemoteDataSource);
 
   final markEntryRemoteDataSource = MarkEntryRemoteDataSource(dataProvider);
-  final resultRepository =
-      ResultRepositoryImpl(databaseService, markEntryRemoteDataSource);
+  final resultRepository = ResultRepositoryImpl(
+    databaseService,
+    markEntryRemoteDataSource,
+  );
 
   runApp(
     MultiProvider(
@@ -94,8 +99,10 @@ void main() {
         ChangeNotifierProvider(create: (_) => RoutineNotifier()),
         ChangeNotifierProvider(create: (_) => ExamsNotifier()),
         ChangeNotifierProvider(
-          create: (_) =>
-              HomeworkNotifier(databaseService, homeworkRepository: homeworkRepository),
+          create: (_) => HomeworkNotifier(
+            databaseService,
+            homeworkRepository: homeworkRepository,
+          ),
         ),
         ChangeNotifierProvider(
           create: (_) => AttendanceNotifier(attendanceRepository),
@@ -141,10 +148,7 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('en'),
-        Locale('bn'),
-      ],
+      supportedLocales: const [Locale('en'), Locale('bn')],
       onGenerateRoute: RouteGenerator.generateRoute,
       initialRoute: RouteGenerator.splashRoute,
     );
